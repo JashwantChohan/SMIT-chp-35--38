1b3c540d1b5f774a8dd30850a1af4af1d66737f4
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-04-25 14:05:55 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// 1. Write a program that displays current date and time in
+// your browser.
+
+// function displayDateTme(){
+//     let now = new Date()
+//     const dateTime = now.toLocaleString()
+//     console.log(dateTime)
+// }
+
+// displayDateTme()
+
+// 2. Write a function that takes first & last name and then it
+// greets the user using his full name.
+
+
+
+// function greeting(firstName, lastName) {
+//     let fullName = firstName + " " + lastName
+//     console.log("Hello " + fullName)
+// }
+
+// greeting("Jashwant", "Chohan")
+
+
+
+// 3. Write a function that adds two numbers (input by user)
+// and returns the sum of two numbers.
+
+// function sum(num1, num2){
+//     const sum = num1 + num2
+//     console.log(sum)
+// }
+
+// sum(2, 2)
