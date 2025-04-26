b4f480d15429fa702eb2340bbbcd939037a6f144
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-04-26 14:07:46 +0500

    Q6 done

--- script.js
+++ script.js
@@ -32,3 +32,53 @@
 // }
 
 // sum(2, 2)
+
+
+// 4. Calculator:
+// Write a function that takes three arguments num1, num2
+// & operator & compute the desired operation. Return and
+// show the desired result in your browser.
+
+// let num1 = parseFloat(+prompt("Enter first number"))
+// let num2 = parseFloat(+prompt("Enter second number"))
+// let operator = prompt("Enter operator like + , - , / , * ")
+
+// function Calculator(num1, num2, operator){
+//     let result 
+//     if(operator === "+"){
+//         result = num1 + num2
+//     }else if(operator === "-"){
+//         result = num1 - num2
+//     }
+//     else if(operator === "/"){
+//         result = num1 / num2
+//     }
+//     else if(operator === "*"){
+//         result = num1 * num2
+//     }
+//     else {
+//         alert("Invalid numbers or operator")
+//         return
+//     }
+
+//     return result
+// }
+// let result = Calculator(num1, num2, operator)
+// console.log(result)
+
+// document.write(`The result of your input is ${result}`)
+
+
+// 5. Write a function that squares its argument.
+
+// function squareRoot(number) {
+//     return number * number
+// }
+
+// let no = parseFloat(+prompt("Enter a number to square"))
+// let result = squareRoot(no)
+// document.write(`Your square root of given number is ${result}`)
+// console.log(result)
+
+// 6. Write a function that computes factorial of a number.
+
